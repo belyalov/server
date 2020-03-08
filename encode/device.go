a625d09d9660087404a8fe94f526b2270e53aed5
Konstantin Belyalov <arsenicus@mail.ru>
2020-03-07 17:03:25 -0800

    [encode] Add MakeReadyToSendDeviceMessage

---
+++ encode/device.go
@@ -0,0 +1,63 @@
+package encode
+
+import (
+	"bytes"
+	"hash/crc32"
+
+	"github.com/golang/protobuf/proto"
+
+	"github.com/open-iot-devices/protobufs/go/openiot"
+	"github.com/open-iot-devices/server/device"
+)
+
+// MakeReadyToSendDeviceMessage makes ready to be send message using device's settings.
+// It does:
+// - Serializes all msgs
+// - Calculates CRC
+// - Makes MessageInfo header
+// - Encrypts all (if enabled on device)
+// - Makes MessageHeader
+// - Writes all messages into buffer
+func MakeReadyToSendDeviceMessage(dev *device.Device, msgs ...proto.Message) ([]byte, error) {
+	// Increase send sequence: In order to be able to filter duplicates
+	// remove device tracks last received sequence and ignores messages
+	// that has already been processed.
+	dev.SequenceSend++
+	info := &openiot.MessageInfo{
+		Sequence: dev.SequenceSend,
+	}
+
+	// Serialize (with optional encryption) all messages:
+	// info, msgs...
+	var serializeBuf bytes.Buffer
+	var err error
+	allMsgs := append([]proto.Message{info}, msgs...)
+
+	switch dev.EncodingType {
+	case openiot.EncryptionType_PLAIN:
+		err = WritePlain(&serializeBuf, allMsgs...)
+	case openiot.EncryptionType_AES_ECB:
+		err = WriteAndEncryptECB(&serializeBuf, dev.Key(), allMsgs...)
+	}
+	// Encrypt / serialize failed
+	if err != nil {
+		return nil, err
+	}
+
+	// Make message header
+	hdr := &openiot.Header{
+		DeviceId: dev.ID,
+		Crc:      crc32.ChecksumIEEE(serializeBuf.Bytes()),
+	}
+
+	// Write all messages into one buffer
+	var buf bytes.Buffer
+	if err := WriteSingleMessage(&buf, hdr); err != nil {
+		return nil, err
+	}
+	if _, err := serializeBuf.WriteTo(&buf); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
---
+++ encode/device_test.go
@@ -0,0 +1,94 @@
+package encode
+
+import (
+	"bytes"
+	"hash/crc32"
+	"testing"
+
+	"github.com/open-iot-devices/protobufs/go/openiot"
+	"github.com/open-iot-devices/server/device"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeviceMessageNoEncryption(t *testing.T) {
+	dev := device.NewDevice(123)
+	dev.SequenceSend = 1
+
+	payload, err := MakeReadyToSendDeviceMessage(dev,
+		&openiot.JoinRequest{
+			Name:         "test1",
+			Manufacturer: "man1",
+		},
+		&openiot.JoinResponse{
+			Timestamp: 555,
+		},
+	)
+	require.NoError(t, err)
+	buf := bytes.NewBuffer(payload)
+
+	// Read message header
+	hdr := &openiot.Header{}
+	err = ReadSingleMessage(buf, hdr)
+	require.NoError(t, err)
+	assert.Equal(t, dev.ID, hdr.DeviceId)
+
+	// Validate CRC
+	crc := crc32.ChecksumIEEE(buf.Bytes())
+	assert.Equal(t, crc, hdr.Crc)
+
+	// Read the rest: MessageInfo, JoinReq, JoinResp
+	msgInfo := &openiot.MessageInfo{}
+	joinReq := &openiot.JoinRequest{}
+	joinResp := &openiot.JoinResponse{}
+	err = ReadPlain(buf, msgInfo, joinReq, joinResp)
+	require.NoError(t, err)
+
+	// Message Info (send sequence is correct)
+	assert.Equal(t, dev.SequenceSend, msgInfo.Sequence)
+
+	// Actual "device" messages
+	assert.Equal(t, "test1", joinReq.Name)
+	assert.Equal(t, "man1", joinReq.Manufacturer)
+	assert.Equal(t, int64(555), joinResp.Timestamp)
+}
+
+func TestDeviceMessageAesECB(t *testing.T) {
+	dev := device.NewDevice(333)
+	dev.SequenceSend = 10
+	// Setup encryption for device
+	dev.SetKey([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})
+	dev.EncodingType = openiot.EncryptionType_AES_ECB
+
+	payload, err := MakeReadyToSendDeviceMessage(dev,
+		&openiot.JoinRequest{
+			Name:         "test2",
+			Manufacturer: "man22",
+		},
+	)
+	require.NoError(t, err)
+	buf := bytes.NewBuffer(payload)
+
+	// Read message header
+	hdr := &openiot.Header{}
+	err = ReadSingleMessage(buf, hdr)
+	require.NoError(t, err)
+	assert.Equal(t, dev.ID, hdr.DeviceId)
+
+	// Validate CRC
+	crc := crc32.ChecksumIEEE(buf.Bytes())
+	assert.Equal(t, crc, hdr.Crc)
+
+	// Read the rest: MessageInfo, JoinReq, JoinResp
+	msgInfo := &openiot.MessageInfo{}
+	joinReq := &openiot.JoinRequest{}
+	err = DecryptAndReadECB(buf, dev.Key(), msgInfo, joinReq)
+	require.NoError(t, err)
+
+	// Message Info (send sequence is correct)
+	assert.Equal(t, dev.SequenceSend, msgInfo.Sequence)
+
+	// Actual "device" messages
+	assert.Equal(t, "test2", joinReq.Name)
+	assert.Equal(t, "man22", joinReq.Manufacturer)
+}
